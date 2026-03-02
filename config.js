79eaf46b11e2aaea4ce241fb3cad921e1851e49b
basnemy <107727404+AlexisFloresCcalli@users.noreply.github.com>
2026-03-01 21:33:11 -0500

    Add Mapbox token to config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // config.js
 // Agrega aquí tu token de Mapbox de forma local y segura
-const MAPBOX_TOKEN = '';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmFzbmVteSIsImEiOiJjbW03MW80YWUwMmNqMnFwcjl1a2E1ZXN1In0.edkYjjcmV65Px9X5TEbJGw';
+
